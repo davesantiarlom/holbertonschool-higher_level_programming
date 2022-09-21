cf172d678c31841e497b5d794dc8d6173cde1fc5
davesantiarlom <david.arias02@usc.edu.co>
2022-09-20 22:45:29 -0500

    Tasks

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new = ""
+    """
+    removes all characters 'c' and 'C' from s
+    """
+    new_str = ""
     for i in my_string:
-        if i is not 'c' and i is not 'C':
-            new += i
-    return new
+        if i not in "cC":
+            new_str += i
+    return (new_str)
