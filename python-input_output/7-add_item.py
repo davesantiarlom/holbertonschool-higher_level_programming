d9600df6a9696d5022f2ddc0d088a50992224afd
David Santiago Arias Lombana <57549591+davesantiarlom@users.noreply.github.com>
2022-10-12 11:21:52 -0500

    Update 7-add_item.py

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,25 +1,16 @@
 #!/usr/bin/python3
-
-from sys import argv
+import sys
 import json
+
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
-'''
-Write a script that adds all
-arguments to a Python list, and then
-save them to a file
-'''
+if __name__ == "__main__":
+    try:
+        json_list = load_from_json_file('add_item.json')
+    except FileNotFoundError:
+        json_list = []
 
-
-argc = len(argv)
-
-filename = 'add_item.json'
-my_list = []
-try:
-    my_list = load_from_json_file(filename)
-except BaseException:
-    pass
-for items in range(1, argc):
-    my_list.append(argv[items])
-save_to_json_file(my_list, filename)
+    for i in range(1, len(sys.argv)):
+        json_list.append(sys.argv[i])
+    save_to_json_file(json_list, "add_item.json")
