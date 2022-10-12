2a14c4690cb3c92d55f7e70553e8fbd47995f1ed
David Santiago Arias Lombana <57549591+davesantiarlom@users.noreply.github.com>
2022-10-12 11:18:26 -0500

    Update 7-add_item.py

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,42 +1,20 @@
 #!/usr/bin/python3
-# -*- coding: utf-8 -*-
-"""
-Created on Tru Jun 3 15:13:37 2022
-@author: David Santiago
-"""
-
+"""Module for saving to json"""
 import json
+import os.path
+import sys
 from sys import argv
 
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
-"""
-This file contains a function that adds
-all arguments to a python list and saves
-to a file
-"""
+filename = "add_item.json"
+json_list = []
 
+if os.path.exists(filename):
+    json_list = load_from_json_file(filename)
 
-save_json = __import__('7-save_to_json_file').save_to_json_file
-load_json = __import__('8-load_from_json_file').load_from_json_file
+for index in argv[1:]:
+    json_list.append(index)
 
-filename = 'add_item.json'
-        """
-        Represents of Student into json format
-        Attributes:
-            attrs (dict): A python object to convert
-        Return:
-            Student class as a json format
-        """
-try:
-    new = load_json(filename)
-except (ValueError, FileNotFoundError):
-    new = []
-        """
-        Represents of Student into json format
-        Attributes:
-            attrs (dict): A python object to convert
-        Return:
-            Student class as a json format
-        """
-new = new + argv[1:]
-save_json(new, filename)
+save_to_json_file(json_list, filename)
