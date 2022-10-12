333cd2d9efece1da7f88ee8ebd0359f6eff2477c
David Santiago Arias Lombana <57549591+davesantiarlom@users.noreply.github.com>
2022-10-11 20:02:01 -0500

    Update 2-append_write.py

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,11 +1,4 @@
 #!/usr/bin/python3
-"""
-This file contains function that appends
-a string at the end of a text file and
-returns number of characters added
-"""
-
-
 def append_write(filename="", text=""):
-    with open(filename, mode="a", encoding="utf-8") as myFile:
-        return (myFile.write(str(text)))
+    with open(filename, 'a+', encoding='utf-8') as f:
+        return f.write(text)
