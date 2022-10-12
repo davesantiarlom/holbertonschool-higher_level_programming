91c622c8b07a19380ec8da685d0d9718f0e058c1
David Santiago Arias Lombana <57549591+davesantiarlom@users.noreply.github.com>
2022-10-12 11:24:23 -0500

    Update 7-add_item.py

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,25 +1,20 @@
 #!/usr/bin/python3
+"""
+Script that adds all arguments to a Python list, and then saves them to a file
+"""
 
 from sys import argv
-import json
-save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+save_to_json_file = __import__("7-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("8-load_from_json_file").load_from_json_file
 
-'''
-Write a script that adds all
-arguments to a Python list, and then
-save them to a file
-'''
+filename = "add_item.json"
 
-
-argc = len(argv)
-
-filename = 'add_item.json'
-my_list = []
 try:
-    my_list = load_from_json_file(filename)
-except BaseException:
-    pass
-for items in range(1, argc):
-    my_list.append(argv[items])
-save_to_json_file(my_list, filename)
+    json_list = load_from_json_file(filename)
+except:
+    json_list = []
+
+for arg in argv[1:]:
+    json_list.append(arg)
+
+save_to_json_file(json_list, filename)
