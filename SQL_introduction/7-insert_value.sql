12f05e82161a1585655f2b0644393133fb39246d
David Santiago Arias Lombana <57549591+davesantiarlom@users.noreply.github.com>
2022-11-10 10:11:07 -0500

    Create 7-insert_value.sql

---
+++ SQL_introduction/7-insert_value.sql
@@ -0,0 +1,7 @@
+-- This script creates a new row in the table of the passed in argument
+-- The table itself must exist before this can work
+
+INSERT INTO first_table VALUES (
+    89,
+    'Holberton School'
+)
