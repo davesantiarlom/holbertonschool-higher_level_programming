2209b4c69f3f3a64c745c2ab8d9f30824abf6473
David Santiago Arias Lombana <57549591+davesantiarlom@users.noreply.github.com>
2022-11-10 10:08:30 -0500

    Create 1-create_database_if_missing.sql

---
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,4 @@
+-- This script creates the database hbtn_0c_0 if it exists
+-- For whatever reason the DB_ID() function does not work. It's probably syntax
+
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0
