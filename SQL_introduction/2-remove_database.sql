e4497f7d66ac4eb8381a5fd39000948547c82d40
David Santiago Arias Lombana <57549591+davesantiarlom@users.noreply.github.com>
2022-11-10 10:09:18 -0500

    Create 2-remove_database.sql

---
+++ SQL_introduction/2-remove_database.sql
@@ -0,0 +1,5 @@
+-- This script deletes the database hbtn_0c_0 if it exists
+-- Interestingly, the DELETE function is not the correct one to use here, as it only deletes content
+-- within the database, not the database itself
+
+DROP DATABASE IF EXISTS hbtn_0c_0
