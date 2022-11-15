036c4741d3cca9bbf5dd96b4f3be264fc8f7f164
David Santiago Arias Lombana <57549591+davesantiarlom@users.noreply.github.com>
2022-11-15 08:24:40 -0500

    Create 14-my_genres.sql

---
+++ SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,9 @@
+-- This script displays the genres for the show Dexter
+-- As this script requires the genre name, title name, and their ids, multiple joins are necessary
+
+SELECT tv_genres.name
+FROM tv_genres
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_shows.title = 'Dexter'
+ORDER BY tv_genres.name ASC;
