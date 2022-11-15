54ce27f5153162861397231f13f6a4ddcee084fd
davesantiarlom <david.arias02@usc.edu.co>
2022-11-15 18:43:30 -0500

    Tasks

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# -*- coding: utf-8 -*-
+
 """
 Created on Wednesday Nov 15 09:05:11 2020
 @author: David Arias
@@ -10,16 +10,18 @@ import sys
 
 if __name__ == '__main__':
     args = sys.argv
-    if len(args) != 4:
+    if len(args) != 5:
         print("Usage: {} username password database_name".format(args[0]))
         exit(1)
     username = args[1]
     password = args[2]
     data = args[3]
+    state_name = args[4]
     db = MySQLdb.connect(host='localhost', user=username,
                          passwd=password, db=data, port=3306)
     cur = db.cursor()
-    num_rows = cur.execute('SELECT * FROM states ORDER BY states.id;')
+    num_rows = cur.execute("SELECT * FROM states WHERE states.name LIKE BINARY\
+                           '{}' ORDER BY states.id;".format(state_name))
     rows = cur.fetchall()
     for row in rows:
         print(row)
