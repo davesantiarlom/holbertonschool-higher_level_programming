13b52cd194f3194d08b771086beba0fb20d4b93c
David Santiago Arias Lombana <57549591+davesantiarlom@users.noreply.github.com>
2022-12-20 18:18:07 -0500

    Update 6-multi_languages_loop.js

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const multilingual = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const multilingual = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 let i;
 
 for (i = 0; i < multilingual.length; i++) {
