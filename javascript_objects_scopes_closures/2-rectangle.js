52bf41963cfbda5d1b1d3546399834b02c1b542e
David Santiago Arias Lombana <57549591+davesantiarlom@users.noreply.github.com>
2022-12-20 19:04:19 -0500

    Update 2-rectangle.js

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -3,9 +3,9 @@
 class Rectangle {
   constructor (w, h) {
     // The instructions want these two conditionals together
-    if (parseInt(w) > 0 && parseInt(h) > 0 ) {
-      this.height = h;
+    if (parseInt(w) > 0 && parseInt(h) > 0) {
       this.width = w;
+      this.height = h;
     }
   }
 }
